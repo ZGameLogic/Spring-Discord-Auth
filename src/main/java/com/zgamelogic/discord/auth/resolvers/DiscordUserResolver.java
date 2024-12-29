bfa01048917e5c88d4e7831faf548ed8bc7f0a71
Ben Shabowski <benshabowski@gmail.com>
2024-12-29 16:25:18 -0600

    Testing

--- src/main/java/com/zgamelogic/discord/auth/resolvers/DiscordUserResolver.java
+++ src/main/java/com/zgamelogic/discord/auth/resolvers/DiscordUserResolver.java
@@ -7,6 +7,7 @@ import com.zgamelogic.discord.auth.exceptions.UnauthorizedException;
 import com.zgamelogic.discord.auth.services.DiscordService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -14,6 +15,7 @@ import org.springframework.web.method.support.ModelAndViewContainer;
 
 import java.util.Optional;
 
+@Component
 public class DiscordUserResolver implements HandlerMethodArgumentResolver {
     private final DiscordService discordService;
     private final AuthDataRepository authDataRepository;
