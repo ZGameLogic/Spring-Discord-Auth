61e7bebc232ed13a3172b3c2f51ebae8d9d5777e
Ben Shabowski <benshabowski@gmail.com>
2026-03-18 14:09:14 -0500

    Update DiscordUser record by removing verified field and bump version to 0.0.6 in pom.xml

--- src/main/java/com/zgamelogic/discord/auth/data/authData/DiscordUser.java
+++ src/main/java/com/zgamelogic/discord/auth/data/authData/DiscordUser.java
@@ -2,7 +2,6 @@ package com.zgamelogic.discord.auth.data.authData;
 
 public record DiscordUser(
         String locale,
-        boolean verified,
         String username,
         String global_name,
         String avatar,
